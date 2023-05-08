96934603c3034f4fe113472d9119342135ea6c64
JosephGlz <joseph220199@gmail.com>
2023-05-08 14:29:46 -0600

    first Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TempSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 179);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "---";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(565, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(281, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 97);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "°C";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(372, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 204);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 369);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Medidor de Temperatura";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace TempSystem
+{
+    public partial class Form1 : Form
+    {
+        SerialPort com = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+            ConnectPort();
+        }
+
+        public void ConnectPort()
+        {
+            bool portfound = false;
+            //=============================================
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(s);
+                com.Close(); // To handle the exception, in case the port isn't found and then they try again...
+
+                com.PortName = s;
+                com.BaudRate = 9600;
+                try
+                {
+                    com.Open();
+                    if (com.IsOpen)
+                    {
+                        portfound = true;
+                    }
+                }
+                catch
+                {
+                    if (!portfound)
+                    {
+                        com.Close();
+                    }
+                }
+            }
+            //=============================================
+
+            //serialPort = new SerialPort("COM6", 9600);
+
+            //try
+            //{
+            //    serialPort.Open();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (com.IsOpen)
+            {
+                string temp = "";
+                int iTemp = 0;
+                try
+                {
+                    temp = com.ReadLine();
+                    temp = temp.Remove(temp.IndexOf("."), (temp.Length - temp.IndexOf(".")));
+                    iTemp = Convert.ToInt32(temp);
+                }
+                catch
+                {
+                    temp = "-";
+                    iTemp = 0;
+                }
+
+                if (iTemp != 0)
+                {
+                    if (iTemp <= 30)
+                    {
+                        pictureBox1.Image = global::TempSystem.Properties.Resources.Frio;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = global::TempSystem.Properties.Resources.caliente;
+                    }
+                    label1.Text = "" + iTemp;
+                }
+            } else
+            {
+                ConnectPort();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
